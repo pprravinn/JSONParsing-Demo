a1d0ab53a6c926d7d9663a25286a8f5f4dc4834b
pravinkamble <pprravinn@gmail.com>
2018-03-24 10:52:35 +0530

    Initial Commit

---
+++ JSONParsing-Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JSONParsing-Demo
+//
+//  Created by Pravin Kamble on 24/03/18.
+//  Copyright © 2018 Pravin Kamble. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
